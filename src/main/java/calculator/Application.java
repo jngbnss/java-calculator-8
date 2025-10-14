414a0f41f7e17f92be8dff2b5db187e5b3945532
jngbn <[user email]>
2025-10-14 19:42:44 +0900

    입력 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
+        Cal cal = new Cal(input);
+        int result = cal.getRet();
     }
 }
---
+++ src/main/java/calculator/Cal.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Cal {
+    private int ret;
+    private int temp;
+    private String input;
+
+    public Cal(String input) {
+        this.input = input;
+    }
+
+    public int getRet() {
+        return ret;
+    }
+}
