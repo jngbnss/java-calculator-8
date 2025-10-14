8dcb93f360364ff5be0fc2160838664014245aaf
jngbn <[user email]>
2025-10-14 19:54:50 +0900

    계산 기능

--- src/main/java/calculator/Cal.java
+++ src/main/java/calculator/Cal.java
@@ -7,7 +7,30 @@ public class Cal {
 
     public Cal(String input) {
         this.input = input;
+        for (Character c : input.toCharArray()) { // 문자열은 바로 character로 순회 못함
+            if(c==','||c==':'){
+                ret+=temp;
+                temp=0;
+            }else{
+                //temp = Integer.parseInt(c);
+                temp = c-'0';
+            }
+        }
+        if(temp!=0) ret+=temp;
     }
+
+//    private int calcul(String input){
+//        for (Character c : input.toCharArray()) { // 문자열은 바로 character로 순회 못함
+//            if(c==','||c==':'){
+//                ret+=temp;
+//                temp=0;
+//            }else{
+//                //temp = Integer.parseInt(c);
+//                temp = c-'0';
+//            }
+//        }
+//        return ret;
+//    }
 
     public int getRet() {
         return ret;
