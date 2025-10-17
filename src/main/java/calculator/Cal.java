32d7f4f6385d1bc8541c0b923a6161668556b568
jngbn <[user email]>
2025-10-17 11:24:04 +0900

    커스텀 구분자 구현 완료

--- src/main/java/calculator/Cal.java
+++ src/main/java/calculator/Cal.java
@@ -7,18 +7,37 @@ public class Cal {
 
     public Cal(String input) {
         this.input = input;
-        for (Character c : input.toCharArray()) { // 문자열은 바로 character로 순회 못함
-            if(c==','||c==':'){
-                ret+=temp;
-                temp=0;
-            }else if(Character.isDigit(c)){
+
+        //커스텀 구분자 있는지 확인
+        char customDelimiter = 0;//0이면 기본 구분자 사용
+        String numbers = input;
+
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\\n");
+            if (newlineIndex == -1) { // 인덱스 위치가 없거나 2번째인 경우 3번째에 개행이 시작되어야함
+                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+                // test input = //\n1,2 or //;;\n1,2
+            }
+            customDelimiter = input.charAt(2); //와 \n 사이의 문자
+            numbers = input.substring(newlineIndex + 2); //숫자 부분만 남기기 1말고 2로받기
+        }
+
+        for (char c : numbers.toCharArray()) { // 문자열은 바로 character로 순회 못함
+            if (c == ',' || c == ':' || (customDelimiter != 0 && c == customDelimiter)) {
+                ret += temp;
+                temp = 0;
+            } else if (Character.isDigit(c)) {
                 //temp = Integer.parseInt(c);
-                temp = c-'0';
-            }else{
+                //temp = c - '0';
+                temp = temp * 10 + (c - '0'); // 여러 자리 숫자도 처리 가능
+            } else {
                 throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
             }
         }
-        if(temp!=0) ret+=temp;
+//        if (temp != 0) {
+//            ret += temp;
+//        }
+        ret += temp;
     }
 
 //    private int calcul(String input){
@@ -36,5 +55,5 @@ public class Cal {
 
     public int getRet() {
         return ret;
-    }
+    }// 이게 필요한가?
 }
