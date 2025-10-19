91e23bce0424801bc3c78d40fd9d031440ecc020
jngbn <[user email]>
2025-10-19 12:59:58 +0900

    feat: 문자열 입력 및 기본 덧셈 기능 구현

--- src/main/java/calculator/Cal.java
+++ src/main/java/calculator/Cal.java
@@ -1,5 +1,27 @@
 package calculator;
 
 public class Cal {
-    
+    private int ret;
+    private int temp;
+    private String input;
+
+    public Cal(String input) {
+        this.input = input;
+
+        String numbers = input;
+
+        for (char c : numbers.toCharArray()) { // 문자열은 바로 character로 순회 못함
+            if (c == ',' || c == ':') {
+                ret += temp;
+                temp = 0;
+            } else if (Character.isDigit(c)) {
+                temp = temp * 10 + (c - '0'); // 여러 자리 숫자도 처리 가능
+            }
+        }
+        ret += temp;
+    }
+
+    public int getRet() {
+        return ret;
+    }// 이게 필요한가?
 }
