2043e61e4e640d57f1839a0372eaca34252eb295
jngbn <[user email]>
2025-10-19 13:03:08 +0900

    feat(application): 입력 및 결과 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        Cal cal = new Cal(input);
+        int result = cal.getRet();
+        System.out.println("결과 : " + result);
     }
 }
 //예: "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6
